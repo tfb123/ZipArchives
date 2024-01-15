ff17caae92744a617cbdabb6c17766f407a3d4a2
tfb123 <mil.xomiak@gmail.com>
2024-01-15 03:44:59 +0200

    Add project files.

---
+++ Zipulia(ZipArchives)/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Zipulia_ZipArchives_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.CreateZipButton = new System.Windows.Forms.Button();
+            this.ExtractZipButton = new System.Windows.Forms.Button();
+            this.ExtractZipButton7z = new System.Windows.Forms.Button();
+            this.CreateZipButton7z = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(24, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Archive";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(189, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(461, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.browseButton.Location = new System.Drawing.Point(674, 26);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(139, 42);
+            this.browseButton.TabIndex = 2;
+            this.browseButton.Text = "...";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(24, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Archive Content";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(29, 132);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(764, 229);
+            this.listBox1.TabIndex = 4;
+            // 
+            // CreateZipButton
+            // 
+            this.CreateZipButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CreateZipButton.Location = new System.Drawing.Point(63, 380);
+            this.CreateZipButton.Name = "CreateZipButton";
+            this.CreateZipButton.Size = new System.Drawing.Size(160, 61);
+            this.CreateZipButton.TabIndex = 5;
+            this.CreateZipButton.Text = "Create Zip from folder";
+            this.CreateZipButton.UseVisualStyleBackColor = true;
+            this.CreateZipButton.Click += new System.EventHandler(this.CreateZipButton_Click);
+            // 
+            // ExtractZipButton
+            // 
+            this.ExtractZipButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ExtractZipButton.Location = new System.Drawing.Point(242, 380);
+            this.ExtractZipButton.Name = "ExtractZipButton";
+            this.ExtractZipButton.Size = new System.Drawing.Size(160, 61);
+            this.ExtractZipButton.TabIndex = 6;
+            this.ExtractZipButton.Text = "Extract Zip to folder";
+            this.ExtractZipButton.UseVisualStyleBackColor = true;
+            this.ExtractZipButton.Click += new System.EventHandler(this.ExtractZipButton_Click);
+            // 
+            // ExtractZipButton7z
+            // 
+            this.ExtractZipButton7z.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ExtractZipButton7z.Location = new System.Drawing.Point(423, 380);
+            this.ExtractZipButton7z.Name = "ExtractZipButton7z";
+            this.ExtractZipButton7z.Size = new System.Drawing.Size(160, 61);
+            this.ExtractZipButton7z.TabIndex = 7;
+            this.ExtractZipButton7z.Text = "Extract Zip to folder 7z";
+            this.ExtractZipButton7z.UseVisualStyleBackColor = true;
+            this.ExtractZipButton7z.Click += new System.EventHandler(this.ExtractZipButton7z_Click);
+            // 
+            // CreateZipButton7z
+            // 
+            this.CreateZipButton7z.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CreateZipButton7z.Location = new System.Drawing.Point(603, 380);
+            this.CreateZipButton7z.Name = "CreateZipButton7z";
+            this.CreateZipButton7z.Size = new System.Drawing.Size(160, 61);
+            this.CreateZipButton7z.TabIndex = 8;
+            this.CreateZipButton7z.Text = "Create Zip from folder 7z";
+            this.CreateZipButton7z.UseVisualStyleBackColor = true;
+            this.CreateZipButton7z.Click += new System.EventHandler(this.CreateZipButton7z_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(29, 454);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(764, 29);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 495);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.CreateZipButton7z);
+            this.Controls.Add(this.ExtractZipButton7z);
+            this.Controls.Add(this.ExtractZipButton);
+            this.Controls.Add(this.CreateZipButton);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button CreateZipButton;
+        private System.Windows.Forms.Button ExtractZipButton;
+        private System.Windows.Forms.Button ExtractZipButton7z;
+        private System.Windows.Forms.Button CreateZipButton7z;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Zipulia(ZipArchives)/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+using System.Threading;
+using SevenZip;
+
+namespace Zipulia_ZipArchives_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string source;
+        string destination;
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog()
+            {
+                Title = "Select Archive",
+                Filter = GetAllSupportedFormats(),
+                Multiselect = false,
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+            };
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = ofd.FileName;
+                ShowArchive(ofd.FileName);
+            }
+        }
+        private void ShowArchive(string path)
+        {
+            listBox1.Items.Clear();
+            ZipArchive zipArchive = ZipFile.OpenRead(path);
+            foreach (var item in zipArchive.Entries)
+            {
+                listBox1.Items.Add(item.FullName);
+            }
+        }
+
+        private void CreateZipButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog()
+            {
+                Description = "Select Folder for ahrchiving",
+                ShowNewFolderButton = false
+            };
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                source = fbd.SelectedPath;
+                SaveFileDialog sfd = new SaveFileDialog()
+                {
+                    Title = "Select where to save ",
+                    Filter = GetAllSupportedFormats(),
+                };
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    destination = sfd.FileName;
+                    Thread thread = new Thread(StartZipProcess);// чтобы работало больше application , чтоб не нагруж сист. (трэды делать ПОЛЕЗНО)
+                    thread.Start();
+                }
+            }
+        }
+        private void StartZipProcess(object obj)
+        {
+            ZipFile.CreateFromDirectory(source,destination, System.IO.Compression.CompressionLevel.Optimal,false);
+            MessageBox.Show("Completed");
+        }
+
+        private void ExtractZipButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog()
+            {
+                Title = "Select Archive",
+                Filter = GetAllSupportedFormats(),
+                Multiselect = false
+            };
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                source = ofd.FileName;
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    destination = fbd.SelectedPath;
+                    ZipFile.ExtractToDirectory(source,destination);
+                    MessageBox.Show("Completed");
+                }
+            }
+        }
+
+        private string GetAllSupportedFormats()
+        {
+            var names = Enum.GetNames(typeof(OutArchiveFormat));
+            string filter = "";
+
+            foreach (var item in names)
+            {
+                if (item.ToLower().Contains("seven"))
+                {
+                    filter += $"{item} file|*.7z|";
+                }
+                else
+                {
+                    filter += $"{item} file|*.{item}|";
+                }
+            }
+            return filter = filter.Remove(filter.LastIndexOf('|'));
+        }
+
+        private void ExtractZipButton7z_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog()
+            {
+                Title = "Select Archive",
+                Filter = GetAllSupportedFormats(),
+                Multiselect = false
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                source = ofd.FileName;
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    destination = fbd.SelectedPath;
+                    SevenZipExtractor.SetLibraryPath(Application.StartupPath + @"\x86\7z.dll");
+                    SevenZipExtractor sevenZipExtractor = new SevenZipExtractor(source);
+                    sevenZipExtractor.Extracting += sevenZipExtracting;
+                    sevenZipExtractor.ExtractionFinished += sevenZipExtractionFinished;
+
+                    sevenZipExtractor.ExtractArchive(destination);
+                }
+            }
+        }
+        void sevenZipExtractionFinished(object sender,EventArgs e)
+        {
+            MessageBox.Show("Completed");
+            progressBar1.Value = 0;
+        }
+
+        void sevenZipExtracting(object sender,ProgressEventArgs e)
+        {
+            progressBar1.Value = e.PercentDone; 
+        }
+
+        private void CreateZipButton7z_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog()
+            {
+                Description = "Select Folder for ahrchiving",
+                ShowNewFolderButton = false
+            };
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                source = fbd.SelectedPath;
+                SaveFileDialog sfd = new SaveFileDialog()
+                {
+                    Title = "Select where to save ",
+                    Filter = GetAllSupportedFormats(),
+                };
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    destination = sfd.FileName;
+                    SevenZipCompressor.SetLibraryPath(Application.StartupPath + @"\x86\7z.dll");
+                    SevenZipCompressor sevenZipCompressor = new SevenZipCompressor();
+                    sevenZipCompressor.Compressing += sevenZipCompressing;
+                    sevenZipCompressor.CompressionFinished += sevenZipCompressorFinisher;
+
+                    sevenZipCompressor.CompressionLevel = SevenZip.CompressionLevel.Ultra;
+                    sevenZipCompressor.CompressionMethod = SevenZip.CompressionMethod.Lzma2;
+                    sevenZipCompressor.CompressionMode = SevenZip.CompressionMode.Create;
+
+                    sevenZipCompressor.CompressDirectory(source, destination);
+                }
+            }
+        }
+        void sevenZipCompressorFinisher(object sender , EventArgs e)
+        {
+            MessageBox.Show("Completed");
+            progressBar1.Value = 0;
+        }
+        //===========================
+        // THREAD 2
+        void sevenZipCompressing(object sender, ProgressEventArgs e)
+        {
+            if (InvokeRequired)
+            {
+                // call function from THREAD 1
+                BeginInvoke(new UpdateProgressBarHandler(UpdateProgressBar), e.PercentDone);
+            }
+        }
+        delegate void UpdateProgressBarHandler(int value);
+        private void UpdateProgressBar(int value)
+        {
+            progressBar1.Value = value;
+        }
+    }
+}
